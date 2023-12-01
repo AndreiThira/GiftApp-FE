a4f1878d1d09ec66187dcf7a6e15b5a5d76465e9
Luke Hemmings <officiallylukehemmings@gmail.com>
2023-12-01 14:46:55 +0000

    Created register page

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "../components/loginForm";
+
+export default function Register() {
+
+  return (
+   <div className="flex h-screen justify-center items-center">
+    <LoginForm />
+   </div>
+  )
+}
